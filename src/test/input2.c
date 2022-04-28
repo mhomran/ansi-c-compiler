ec6c00b629c66171ed7afbcdb9c42443f8de9713
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2022-04-29 01:54:11 +0200

    Edit: tabs to spaces and add new test cases

---
+++ src/test/input2.c
@@ -0,0 +1,12 @@
+/**
+ * @file input2.c
+ * @author Mohamed Hassanin Mohamed
+ * @brief This is a test file to test my ANSI C parser and lexer.
+ * @version 0.1
+ * @date 2022-04-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+const const int const int a;
---
+++ src/test/input3.c
@@ -0,0 +1,10 @@
+int x, y;
+f(int x, int a) {
+  int b;
+  y = x+ a*b;
+  if(y < 5) {
+    int a;
+    y = x + a*b;
+  }
+  y = x + a*b;
+}
